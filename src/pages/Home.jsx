9b25a2a7d217f052b4c500092f1e9799aabbd590
moralesvictorr <vmmorales@unicauca.edu.co>
2023-03-04 16:02:11 -0500

    feat: Rol reendering added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-import {logout} from '../features/auth/authSlice'
- 
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../features/auth/authSlice'
+
 const Home = () => {
   const dispatch = useDispatch()
+  const rol  = useSelector(state => state.auth.user.rol)
+
   const handleClick = () => {
     dispatch(logout())
   }
   return (
     <div>
       <button onClick={handleClick}>Cerrar Sesion</button>
+      { rol === "professor" && <h1>Home Profe</h1> }
+      { rol === "student" && <h1>Home Estudiante</h1> }
+      { rol === "admin" && <h1>Home Admin</h1> }
     </div>
   )
 }
