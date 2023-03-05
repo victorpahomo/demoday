5e079b987f6cdd30e5d3f647766b461d7996ef97
moralesvictorr <vmmorales@unicauca.edu.co>
2023-03-04 23:58:24 -0500

    feat: dataSlice added

---
+++ src/features/data/dataSlice.js
@@ -0,0 +1,82 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { auth, db } from '../../api/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+const initialState = {
+  user: null,
+  error: null,
+  loading: true,
+};
+
+const dataSlice = createSlice({
+  name: 'data',
+  initialState,
+  reducers: {
+    getDataStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    loginUserSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+      state.error = null;
+    },
+    loginUserFailure: (state) => {
+      state.loading = false;
+    },
+    signUpStart: (state, action) => {
+      state.loading = true;
+      state.error = null;
+    },
+    signUpSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+      state.error = null;
+    },
+    signUpFailure: (state) => {
+      state.loading = false;
+    },
+    logout(state) {
+      state.user = null;
+      state.error = null;
+      state.loading = false;
+    },
+
+  },
+});
+
+export const { loginUserStart, loginUserSuccess, loginUserFailure, logout, signUpStart, signUpSuccess, signUpFailure } =
+  dataSlice.actions;
+
+export const getRol = async (uid) => {
+  const docRef = doc(db, `users/${uid}`);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    console.log('Document data:', docSnap.data().rol);
+    return docSnap.data().rol;
+  } else {
+    console.log('No such document!');
+  }
+};
+
+export const initializeSession = () => (dispatch) => {
+  let currentUserData = null;
+  const unsubscribe = ondataStateChanged(data, (currentUser) => {
+    if (currentUser) {
+      getRol(currentUser.uid).then((rol) => {
+        currentUserData = {
+          uid: currentUser.uid,
+          email: currentUser.email,
+          rol: rol,
+        };
+        console.log(currentUserData);
+        dispatch(loginUserSuccess(currentUserData));
+      });
+    } else {
+      dispatch(logout());
+    }
+  });
+return unsubscribe;
+};
+
+export default dataSlice.reducer;
