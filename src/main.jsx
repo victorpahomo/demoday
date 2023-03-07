31663ef08cc5f0593fb66758d110ef10548ceee8
JulioJavier <01juliomtz@gmail.com>
2023-03-07 12:12:02 -0500

    feat: header completed

--- src/main.jsx
+++ src/main.jsx
@@ -4,40 +4,12 @@ import App from "./router/App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
-import { BrowserRouter, Router } from "react-router-dom";
-import Sidebar from "./layout/Sidebar";
-const items = [
-  {
-    label: "Home",
-    href: "/home",
-    top: "10px",
-    left: "10px",
-    height: "50px",
-    width: "50px",
-  },
-  {
-    label: "About",
-    href: "/about",
-    top: "70px",
-    left: "10px",
-    height: "50px",
-    width: "50px",
-  },
-  {
-    label: "Contact",
-    href: "/contact",
-    top: "130px",
-    left: "10px",
-    height: "50px",
-    width: "50px",
-  },
-];
+import { BrowserRouter} from "react-router-dom";
+
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <div className="flex w-screen h-screen  bg-primary-azulCeleste5 dark:bg-black p-5 ">
-    <BrowserRouter>
-    <Sidebar items={items} />
-    </BrowserRouter>
-    <Provider store={store}>
+     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
