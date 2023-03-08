4b97b97dae209f08e92b27741026c00cb18d6106
Wildelmy <wildelmycolina@gmail.com>
2023-03-07 19:07:54 -0500

    create block novedades

--- src/components/home/RolHome.jsx
+++ src/components/home/RolHome.jsx
@@ -1,7 +1,16 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../features/auth/authSlice";
-import { getUSerData, getAllUSersData, getGroupData, getAllGroupsData, getGlobalNewsData, getGroupNewsData, getCoursesData, getContributionsData, } from "../../services/dataFirebaseService";
+import {
+  getUSerData,
+  getAllUSersData,
+  getGroupData,
+  getAllGroupsData,
+  getGlobalNewsData,
+  getGroupNewsData,
+  getCoursesData,
+  getContributionsData,
+} from "../../services/dataFirebaseService";
 
 import Calendar from "../../components/home/calendar/CalendarFull";
 const RolHome = () => {
@@ -12,10 +21,14 @@ const RolHome = () => {
   };
   let status = true;
   return (
-      <div className="body w-full h-5/6 bg-white rounded-b-lg">
-        <h1>Bienvenido, {useSelector((state) => state.auth.user.rol)}</h1>
-        {/* VISTA PROFESSOR */}
-        {rol === "professor" && <div className="">
+    <div className="body px-24 w-full h-5/6 bg-white rounded-b-lg">
+      <h1 className="py-5">
+        ¡Hola, {useSelector((state) => state.auth.user.rol)} bienvenido a Code
+        LMS!
+      </h1>
+      {/* VISTA PROFESSOR */}
+      {rol === "professor" && (
+        <div className="">
           <section className="_Novedades">
             {/* Componente para novedades */}
           </section>
@@ -25,9 +38,11 @@ const RolHome = () => {
           <section className="_Proceso">
             {/* Componente para proceso */}
           </section>
-        </div>}
-        {/* VISTA STUDENT */}
-        {rol === "student" && <div className="">
+        </div>
+      )}
+      {/* VISTA STUDENT */}
+      {rol === "student" && (
+        <div className="">
           <section className="_Novedades">
             {/* Componente para novedades */}
           </section>
@@ -37,17 +52,30 @@ const RolHome = () => {
           <section className="_Proceso">
             {/* Componente para proceso */}
           </section>
-        </div>}
-        {/* VISTA ADMIN */}
+        </div>
+      )}
+      {/* VISTA ADMIN */}
 
-        {rol === "admin" && <div className="">
-          <section className="_Novedades bg-white">
+      {rol === "admin" && (
+        <div className="">
+          <section className="_Novedades bg-white py-5 rounded-2xl border-4">
             {/* Componente para novedades */}
-            <h1>Novedad</h1>
+            <div>
+              <div className="flex justify-around">
+                <h1 className="font-medium">Novedades</h1>
+                <h1 className="font-medium">Link a la proxima clase</h1>
+              </div>
+              <h1 className="mt-5 text-3xl font-medium text-center">
+                Ultima novedad
+              </h1>
+              <div className="flex justify-end">
+                <span className="btn mr-20">ver mas</span>
+              </div>
+            </div>
           </section>
 
-          <section className="_Calendario flex items-center justify-center bg-white">
-            <div className="w-1/2 ">
+          <section className="mt-10 _Calendario flex items-center justify-center bg-white">
+            <div className="w-full">
               <Calendar />
             </div>
           </section>
@@ -56,10 +84,9 @@ const RolHome = () => {
             {/* Componente para proceso */}
             <h1>Proceso</h1>
           </section>
-        </div>}
-
-      </div>
-  
+        </div>
+      )}
+    </div>
   );
 };
 
