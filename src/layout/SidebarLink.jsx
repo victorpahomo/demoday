c4810ece13f2ee2b537c5c38c3bb4430c4f0ec58
JulioJavier <01juliomtz@gmail.com>
2023-03-07 22:45:15 -0500

    feat: update styles sidebar

--- src/layout/SidebarLink.jsx
+++ src/layout/SidebarLink.jsx
@@ -2,23 +2,35 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 const SidebarLink = ({ to, icon: Icon, text, showText }) => {
   const [hoverIcon, setHoverIcon] = useState("fill-white");
-//   const [showText, setShowText] = useState(false);
+  //   const [showText, setShowText] = useState(false);
 
   return (
     <Link
       to={to}
-      className="flex h-10 w-36 justify-end items-center rounded-l-full bg-primary-azulCeleste5 p-1 text-center ease-in-out duration-200 font-semibold text-white hover:text-black hover:bg-white"
+      className="flex h-10 w-36 justify-end items-center rounded-l-full bg-primary-azulCeleste5 p-1 text-center ease-in-out duration-200 font-semibold text-white hover:text-primary-azulCeleste5 hover:bg-white"
       onMouseEnter={() => {
-        setHoverIcon("fill-black");
-        
+        setHoverIcon("fill-primary-azulCeleste5");
       }}
       onMouseLeave={() => {
         setHoverIcon("fill-white");
-        
       }}
     >
-      <Icon className={`ease-in-out duration-200 w-8 pr-3 ${hoverIcon}`} />
-      {showText && <span>{text}</span>}
+      {showText ? (
+        <div className="flex justify-start w-32">
+          <Icon className={`ease-in-out duration-200 w-8 pr-3 ${hoverIcon}`} />
+          {showText && <span>{text}</span>}
+        </div>
+      ) : (
+        <Icon className={`ease-in-out duration-200 w-8 pr-3 ${hoverIcon}`} />
+      )}
+      {/* {!showText && (
+        <Icon className={`ease-in-out duration-200 w-8 pr-3 ${hoverIcon}`} />
+      )} */}
+
+      {/* <div className="flex justify-start w-32">
+        <Icon className={`ease-in-out duration-200 w-8 pr-3 ${hoverIcon}`} />
+        {showText && <span>{text}</span>}
+      </div> */}
     </Link>
   );
 };
