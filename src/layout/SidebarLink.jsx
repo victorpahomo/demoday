fb2f07b45f1c3a173fff094eb26b3ace6737b71c
moralesvictorr <vmmorales@unicauca.edu.co>
2023-03-09 10:37:27 -0500

    feat: solving bug color on Sidebar option selected

--- src/layout/SidebarLink.jsx
+++ src/layout/SidebarLink.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 
 const SidebarLink = ({ to, icon: Icon, text, showText, page }) => {
   let [hoverIcon, setHoverIcon] = useState("fill-white");
-  let linkStyles = "flex h-10 w-36 justify-end items-center rounded-l-full bg-primary-azulCeleste5 p-1 text-center ease-in-out duration-200 font-semibold text-white hover:text-primary-azulCeleste5 hover:bg-white";
+  let linkStyles = "text-white hover:text-primary-azulCeleste5 hover:bg-white flex h-10 w-36 justify-end items-center rounded-l-full bg-primary-azulCeleste5 p-1 text-center ease-in-out duration-100 font-semibold";
   if (page === text) {
-    linkStyles += " bg-white text-primary-azulCeleste5 " 
+    linkStyles = "text-primary-azulCeleste5 hover:text-primary-azulCeleste5 flex h-10 w-36 justify-end items-center rounded-l-full bg-white p-1 text-center ease-in-out duration-100 font-semibold" 
     hoverIcon = "fill-primary-azulCeleste5"
   }
   return (
