e0f353ce51b9f841b69181244241697eb7c26674
moralesvictorr <vmmorales@unicauca.edu.co>
2023-03-14 19:32:40 -0500

    Chaged logic

--- src/components/notification/NotificationContainer.jsx
+++ src/components/notification/NotificationContainer.jsx
@@ -39,75 +39,89 @@ const NotificationContainer = () => {
     handleGetNotifications();
   }, [])
 
-  const [notificaciones, setNotificaciones] = useState(useSelector((state) => state.data.groupNotifications));
-   const difference = notificaciones?.filter(
+  /*    const difference = notificaciones?.filter(
     (item) => !notificaciones.some((n) => n.id === item.id)
-  );
-    /* lastNotification !== useSelector((state) => state.data.user.lastNotification){
-      mostrarAlerta()
-      setLastNotifcation(data.user.lastNotificationUSer ==  lastNotification)
-    } */
+    ); */
+  /* lastNotification !== useSelector((state) => state.data.user.lastNotification){
+    mostrarAlerta()
+    setLastNotifcation(data.user.lastNotificationUSer ==  lastNotification)
+  } */
 
-
-  if (difference?.length) {
-    setNotificaciones([...notificaciones, ...difference]);
-    // alert("Hay nuevas notificaciones");
-    toast.custom((t) => (
-      <div
-        className={`${t.visible ? "animate-enter" : "animate-leave"
-          } mt-2 max-w-md w-full bg-gray-200 drop-shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
-      >
-        <div className="flex-1 w-0 p-4">
-          <div className="flex items-start">
-            <div className="flex-shrink-0 pt-0.5"></div>
-            <div className="ml-3 flex-1">
-              <h2 className="text-base text-left font-semibold text-gray-900">
-                {difference[0].title}
-              </h2>
-              <p className="text-left mt-1 text-sm text-gray-500">
-                {difference[0].message}
-              </p>
-              <hr className="mt-1" />
-              <div className="mt-1 flex justify-between text-xs">
-                <p>{difference[0].author}</p>
-                <p>{difference[0].date}</p>
+  /*     const [notificaciones, setNotificaciones] = useState(useSelector((state) => state.data.groupNotifications));
+      const userLastNotification = "useSelector((state) => state.data.user.lastNotification)"
+      useEffect(() => {
+      let lastNotification = notificaciones?.reduce(function (prev, current) {
+        return (prev.date > current.date) ? prev : current
+      })
+    
+      if (lastNotification > userLastNotification) {
+        setNotificaciones([...notificaciones, ...difference]);
+        // alert("Hay nuevas notificaciones");
+        toast.custom((t) => (
+          <div
+            className={`${t.visible ? "animate-enter" : "animate-leave"
+              } mt-2 max-w-md w-full bg-gray-200 drop-shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+          >
+            <div className="flex-1 w-0 p-4">
+              <div className="flex items-start">
+                <div className="flex-shrink-0 pt-0.5"></div>
+                <div className="ml-3 flex-1">
+                  <h2 className="text-base text-left font-semibold text-gray-900">
+                    {lastNotification.title}
+                  </h2>
+                  <p className="text-left mt-1 text-sm text-gray-500">
+                    {lastNotification.message}
+                  </p>
+                  <hr className="mt-1" />
+                  <div className="mt-1 flex justify-between text-xs">
+                    <p>{lastNotification.author}</p>
+                    <p>{lastNotification.date}</p>
+                  </div>
+                </div>
               </div>
             </div>
+            <div className="flex border-l border-gray-200">
+              <button
+                onClick={() => toast.dismiss(t.id)}
+                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-red-600 hover:text-red-500 focus:outline-none focus:ring-2 focus:ring-red-500"
+              >
+                Close
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="flex border-l border-gray-200">
-          <button
-            onClick={() => toast.dismiss(t.id)}
-            className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-red-600 hover:text-red-500 focus:outline-none focus:ring-2 focus:ring-red-500"
-          >
-            Close
-          </button>
-        </div>
-      </div>
-    ));
-  } 
+        ));
+      }
+  
+    }, [loading])
+     */
 
   return (
     <>
-      {!loading? <div
+      <h1>hola</h1>
+      {/*       {!loading ? <div
+        id="dropdown_functionality"
         className="dropdown dropdown-end tooltip tooltip-bottom"
         data-tip="Notificaciones"
       >
         <Toaster />
         <label
+          id="button"
           tabIndex={0}
           className="btn btn-ghost btn-circle btn-sm bg-slate-200 hover:bg-slate-300 indicator"
         >
-          <span className="indicator-item text-white badge bg-red-500 border-none">
+          <span
+            id="indicator"
+            className="indicator-item text-white badge bg-red-500 border-none">
             {notificaciones.length}
           </span>
           <BellIcon className=" w-4 h-4 fill-black" />
         </label>
         <div
+          id="dropdown_content"
           tabIndex={0}
           className="mt-3 p-2 drop-shadow-xl  menu menu-compact dropdown-content bg-base-100 rounded-box w-80"
         >
-          <h2 className="font-bold text-lef p-2">Notificaciones</h2>
+          <h2 id="dropdown_title" className="font-bold text-lef p-2">Notificaciones</h2>
           <hr />
           <div className="overflow-y-auto h-60">
             {notificaciones.map((notification) => (
@@ -134,7 +148,8 @@ const NotificationContainer = () => {
           </a>
         </div>
       </div> : <h1>cargando</h1>}
-    </>
-  );
+    */}
+    </> 
+      );
 };
-export default NotificationContainer;
+      export default NotificationContainer;
