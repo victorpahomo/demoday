17788feef52aaa0c71eea22c00ee83d2fa2e1d4d
JulioJavier <01juliomtz@gmail.com>
2023-03-16 13:26:11 -0500

    feat: added skeleton loader of Carousel

---
+++ src/components/skeletonLoaders/SkeletonCarousel.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SkeletonCarousel = () => {
+  return (
+    <div className='w-full h-36 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]'></div>
+  )
+}
+
+export default SkeletonCarousel
