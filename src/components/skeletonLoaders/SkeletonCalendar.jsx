db84556814c30f0181f1f677397acaf66665a16a
JulioJavier <01juliomtz@gmail.com>
2023-03-16 13:26:56 -0500

    feat: added skeleton loader of Calendar

---
+++ src/components/skeletonLoaders/SkeletonCalendar.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+const SkeletonCalendar = () => {
+  return (
+    <div className="flex flex-col w-full h-screen bg-gray-200 animate-[pulse_0.4s_ease-in-out_infinite] p-1 rounded-lg">
+      <div className="flex  justify-between">
+        <div className="flex flex-col w-26 gap-2">
+          <div className="w-24 h-10 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite] rounded-lg"></div>
+          <div className="w-36 h-10 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite] rounded-lg"></div>
+        </div>
+
+        <div className="w-52 h-10 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite] rounded-lg mt-6"></div>
+        <div className="w-44 h-10 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite] rounded-lg"></div>
+      </div>
+      <div className="flex flex-wrap mt-5 justify-around gap-1">
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+        <div className="w-36 h-20 bg-gray-300 animate-[pulse_0.4s_ease-in-out_infinite]"></div>
+
+      </div>
+    </div>
+  );
+};
+
+export default SkeletonCalendar;
