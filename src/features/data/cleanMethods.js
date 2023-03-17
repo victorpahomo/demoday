b28597ad8aa2f2f3b4aaa8d810ce541563aac622
moralesvictorr <vmmorales@unicauca.edu.co>
2023-03-17 18:56:11 -0500

    added cleaning methods

---
+++ src/features/data/cleanMethods.js
@@ -0,0 +1,7 @@
+export {clean as cleanContribution} from "./contributionSlice";
+export {clean as cleanCourse} from "./courseSlice";
+export {clean as cleanCurriculum} from "./curriculumSlice";
+export {clean as cleanGrade} from "./gradeSlice";
+export {clean as cleanGroup} from "./groupSlice";
+export {clean as cleanNews} from "./newsSlice";
+export {clean as cleanUser } from "./userSlice";
