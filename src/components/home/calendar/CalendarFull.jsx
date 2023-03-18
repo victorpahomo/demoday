317f6a2632aaa334d6b9f24a194d5a07376790bd
moralesvictorr <vmmorales@unicauca.edu.co>
2023-03-17 23:11:02 -0500

    refactoring Calendar & HomeStudent

--- src/components/home/calendar/CalendarFull.jsx
+++ src/components/home/calendar/CalendarFull.jsx
@@ -12,94 +12,104 @@ import { SkeletonCalendar } from "../../skeletonLoaders";
 
 const Calendar = () => {
   const dispatch = useDispatch();
+  // Loaders
+  const groupFetchStatus = useSelector((state) => state.group.loading);
+  // Data
+  const arrayRecurringEvent = useSelector((state) => state.group.group?.recurring_todo);
+  // Calendar
   const [editModal, setEditModal] = useState(false);
   const [showModal, setShowModal] = useState(false);
   const [dateSelected, setDateSelected] = useState("");
   const [eventSelected, setEventSelected] = useState("")
   const [events, setEvents] = useState(useSelector((state) => state.user.user?.todo));
-  const groupState = useSelector((state) => state.group.loading);
-  const recurringEvent = useSelector((state) => state.group.group.recurring_todo);
+  const [eventSources, setEventSources] = useState([])
 
   /* ----------------- CONTENT ---------------------- */
-  let content;
-  if (groupState === "pending") {
-    content = <SkeletonCalendar/>
-  } else if (groupState === "fulfilled") {
-    const eventSources = [
-      {
-        events: recurringEvent.map((event) => ({
-          ...event,
-          rrule: {
-            ...event?.rrule,
-            dtstart: new Date(Date.parse(event?.rrule?.dtstart)),
-            until: new Date(Date.parse(event?.rrule?.until)),
-          },
-          extendedProps: {
-            ...event?.extendedProps,
-            startTime: new Date(event?.rrule?.dtstart),
-            endTime: new Date(event?.rrule?.until),
-          },
-        })),
-      },
-    ];
-    const handleClose = () => {
-      setShowModal(false);
-      setEditModal(false);
+  useEffect(() => {
+    if (groupFetchStatus === "fulfilled") {
+      setEventSources([
+        {
+          events: arrayRecurringEvent.map((event) => ({
+            ...event,
+            rrule: {
+              ...event?.rrule,
+              dtstart: new Date(Date.parse(event?.rrule?.dtstart)),
+              until: new Date(Date.parse(event?.rrule?.until)),
+            },
+            extendedProps: {
+              ...event?.extendedProps,
+              startTime: new Date(event?.rrule?.dtstart),
+              endTime: new Date(event?.rrule?.until),
+            },
+          })),
+        },
+      ])
     }
-    const handleShow = () => setShowModal(true);
-    const handleEdit = () => setEditModal(true);
+  }, [groupFetchStatus]);
 
-
-
-    const handleSave = () => {
-      setEvents([...events, { title, dateSelected }]);
-      setDateSelected("");
-      handleClose();
-    };
-
-    const handleDateClick = (daySelected) => {
-      handleShow();
-      setDateSelected(daySelected.date);
-
-    };
-    const handleEventClick = (eventInfo) => {
-      handleEdit();
-      setEventSelected(eventInfo.event);
-
-    };
-    content = (
-      <>
-        <FullCalendar
-          plugins={[dayGridPlugin, interactionPlugin, timegrid, rrulePlugin]}
-          locale={esLocale}
-          height={"auto"}
-          initialView="dayGridMonth"
-          selectable={true}
-          editable={true}
-          selectMirror={true}
-          events={events}
-          eventSources={eventSources}
-          dateClick={handleDateClick}
-          eventClick={handleEventClick}
-          headerToolbar={{
-            left: "prev,next today",
-            center: "title",
-            right: "dayGridMonth,timeGridWeek,timeGridDay",
-          }}
-        />
-
-        {editModal && <ModalCalendar edit={true} event={eventSelected} handleSave={handleSave} handleClose={handleClose} />}
-        {showModal && <ModalCalendar edit={false} handleSave={handleSave} handleClose={handleClose} />}
-      </>
-    );
-  } else if (groupState === "rejected") {
-    content = <h1>Ha ocurrido un error</h1>
+  const handleClose = () => {
+    setShowModal(false);
+    setEditModal(false);
   }
+  const handleShow = () => setShowModal(true);
+  const handleEdit = () => setEditModal(true);
+
+
+
+  const handleSave = () => {
+    setEvents([...events, { title, dateSelected }]);
+    setDateSelected("");
+    handleClose();
+  };
+
+  const handleDateClick = (daySelected) => {
+    handleShow();
+    setDateSelected(daySelected.date);
+
+  };
+  const handleEventClick = (eventInfo) => {
+    handleEdit();
+    setEventSelected(eventInfo.event);
+
+  };
   /* ------------------------------------------------ */
 
   return (
     <>
-      {content}
+      {groupFetchStatus === "pending" ?//Loading
+        (
+          <SkeletonCalendar />
+        )
+        :
+        groupFetchStatus === "rejected" ?//Error
+          (<h1>Hubo un error</h1>)
+          : //Success
+          (
+            <>
+              <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin, timegrid, rrulePlugin]}
+                locale={esLocale}
+                height={"auto"}
+                initialView="dayGridMonth"
+                selectable={true}
+                editable={true}
+                selectMirror={true}
+                events={events}
+                eventSources={eventSources}
+                dateClick={handleDateClick}
+                eventClick={handleEventClick}
+                headerToolbar={{
+                  left: "prev,next today",
+                  center: "title",
+                  right: "dayGridMonth,timeGridWeek,timeGridDay",
+                }}
+              />
+
+              {editModal && <ModalCalendar edit={true} event={eventSelected} handleSave={handleSave} handleClose={handleClose} />}
+              {showModal && <ModalCalendar edit={false} handleSave={handleSave} handleClose={handleClose} />}
+            </>
+          )
+      }
     </>
   )
 };
--- src/components/home/news/Carousel.jsx
+++ src/components/home/news/Carousel.jsx
@@ -1,99 +1,35 @@
-import React from "react";
+import { useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { Link } from "react-router-dom";
-
-const noticias = [
-  {
-    id: 1,
-    title: "Titular de la noticia 1",
-    role: "student",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta optio quia neque cupiditate voluptate doloribus natus iusto eveniet quisquam quam mollitia quasi, itaque harum est nulla amet, aliquam, pariatur tempora.",
-  },
-  {
-    id: 2,
-    role: "student",
-    title: "Titular de la noticia 2",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta optio quia neque cupiditate voluptate doloribus natus iusto eveniet quisquam quam mollitia quasi, itaque harum est nulla amet, aliquam, pariatur tempora.",
-  },
-  {
-    id: 3,
-    role: "student",
-    title: "Titular de la noticia 3",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta optio quia neque cupiditate voluptate doloribus natus iusto eveniet quisquam quam mollitia quasi, itaque harum est nulla amet, aliquam, pariatur tempora.",
-  },
-  {
-    id: 4,
-    role: "professor",
-    title: "Titular de la noticia 4",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta optio quia neque cupiditate voluptate doloribus natus iusto eveniet quisquam quam mollitia quasi, itaque harum est nulla amet, aliquam, pariatur tempora.",
-  },
-  {
-    id: 5,
-    role: "professor",
-    title: "Titular de la noticia 5",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta optio quia neque cupiditate voluptate doloribus natus iusto eveniet quisquam quam mollitia quasi, itaque harum est nulla amet, aliquam, pariatur tempora.",
-  },
-  {
-    id: 6,
-    role: "student",
-    title: "Titular de la noticia 6",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta optio quia neque cupiditate voluptate doloribus natus iusto eveniet quisquam quam mollitia quasi, itaque harum est nulla amet, aliquam, pariatur tempora.",
-  },
-  {
-    id: 7,
-    role: "admin",
-    title: "Titular de la noticia 7",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta optio quia neque cupiditate voluptate doloribus natus iusto eveniet quisquam quam mollitia quasi, itaque harum est nulla amet, aliquam, pariatur tempora.",
-  },
-  {
-    id: 8,
-    role: "professor",
-    title: "Titular de la noticia 8",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta optio quia neque cupiditate voluptate doloribus natus iusto eveniet quisquam quam mollitia quasi, itaque harum est nulla amet, aliquam, pariatur tempora.",
-  },
-  {
-    id: 9,
-    role: "student",
-    title: "Titular de la noticia 9",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta optio quia neque cupiditate voluptate doloribus natus iusto eveniet quisquam quam mollitia quasi, itaque harum est nulla amet, aliquam, pariatur tempora.",
-  },
-  {
-    id: 10,
-    role: "student",
-    title: "Titular de la noticia 10",
-    description:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta optio quia neque cupiditate voluptate doloribus natus iusto eveniet quisquam quam mollitia quasi, itaque harum est nulla amet, aliquam, pariatur tempora.",
-  },
-];
-
-const banner = [
-  "https://res.cloudinary.com/duxlfjdym/image/upload/v1678597940/Demoday/banner1_h5ivq1.jpg",
-  "https://res.cloudinary.com/duxlfjdym/image/upload/v1678597940/Demoday/banner2_k6fse6.jpg",
-  "https://res.cloudinary.com/duxlfjdym/image/upload/v1678597940/Demoday/banner3_msvmbd.jpg",
-  "https://res.cloudinary.com/duxlfjdym/image/upload/v1678597940/Demoday/banner4_sglmxa.jpg",
-  "https://res.cloudinary.com/duxlfjdym/image/upload/v1678597940/Demoday/banner5_yaepxt.jpg",
-];
+import { getAllNewsData } from "../../../services/dataFirebaseService";
+import { useDispatch, useSelector } from "react-redux";
+import { SkeletonCarousel } from "../../skeletonLoaders";
 
 const Carousel = () => {
-  const settings = {
+  const dispatch = useDispatch();
+  // Loaders
+  const newsFetchStatus = useSelector((state) => state.news.loading);
+  // Data
+  const arrayNews = useSelector((state) => state.news?.allNews);
+
+  // Get news data
+  useEffect(() => {
+    dispatch(getAllNewsData())
+  }, [dispatch])
+
+  const carouselSettings = {
+    /*
+    vertical: true,
+    verticalSwiping: true,
+    swipToSlide: true, */
     dots: true,
     infinite: true,
     speed: 1000,
-    slidesToShow: 3, // por defecto mostrar 2 diapositivas
+    slidesToShow: 1, // por defecto mostrar 3 diapositivas
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 3000, // 5 segundos
+    autoplaySpeed: 4000, // 4 segundos
     responsive: [
       {
         breakpoint: 640, // ajustar la cantidad de diapositivas mostradas en pantallas pequeñas
@@ -104,79 +40,29 @@ const Carousel = () => {
     ],
   };
 
-
-  // dots: true,
-  // infinite: true,
-  // speed: 500,
-  // slidesToShow: 2,
-  // slidesToScroll: 2,
-  // autoplay: true,
-  // autoplaySpeed: 3000, // 5 segundos
-  // let rol = "professor";
   return (
-    <div className="overflow-hidden w-full bg-black">
-      <Slider {...settings}>
-        {banner.map((linkImg, index) => (
-          <img key={index} src={linkImg} alt="" className="h-36 bg" />
-        ))}
-      </Slider>
-    </div>
-    // <div className=" bg-white rounded-lg shadow-md">
-    //   <Slider {...settings}>
-    //     {noticias.map((noticia, index) => (
-    //       <Link key={index} to="training">
-    //         <div className="flex items-center p-1 bg-slate-10 gap-1">
-    //           <div
-    //             className={
-    //               (noticia.role === "student" &&
-    //                 "w-3 h-28 rounded-full bg-blue-200") ||
-    //               (noticia.role === "professor" &&
-    //                 "w-3 h-28 rounded-full bg-yellow-500") ||
-    //               (noticia.role === "admin" &&
-    //                 "w-3 h-28 rounded-full bg-red-800")
-    //             }
-    //           ></div>
-    //           <div className="flex flex-col">
-    //             <div className="flex gap-2">
-    //               <span
-    //                 className={
-    //                   (noticia.role === "student" &&
-    //                     "flex bg-blue-100 text-blue-400 rounded-lg w-auto text-xs items-center justify-center pr-1 pl-1") ||
-    //                   (noticia.role === "professor" &&
-    //                     "flex bg-yellow-100 text-yellow-500 rounded-lg w-auto text-xs items-center justify-center pr-1 pl-1") ||
-    //                   (noticia.role === "admin" &&
-    //                     "flex bg-red-100 text-red-800 rounded-lg w-auto text-xs items-center justify-center pr-1 pl-1")
-    //                 }
-    //               >
-    //                 <p>
-    //                   {(noticia.role === "student" && "Estudiante") ||
-    //                     (noticia.role === "professor" && "Profesor") ||
-    //                     (noticia.role === "admin" && "ADMIN")}
-    //                 </p>
-    //               </span>
-    //               <h2
-    //                 href="#"
-    //                 className={
-    //                   (noticia.role === "student" &&
-    //                     "text-blue-500 hover:text-blue-700 font-semibold") ||
-    //                   (noticia.role === "professor" &&
-    //                     "text-yellow-500 hover:text-blue-700 font-semibold") ||
-    //                   (noticia.role === "admin" && "text-red-800 font-semibold")
-    //                 }
-    //               >
-    //                 {noticia.title}
-    //               </h2>
-    //             </div>
-    //             <p className="text-gray-500 text-sm p-2">
-    //               {" "}
-    //               {noticia.description}{" "}
-    //             </p>
-    //           </div>
-    //         </div>
-    //       </Link>
-    //     ))}
-    //   </Slider>
-    // </div>
+    <section id="studentNews" className="p-1 w-full">
+      <h2 className="text-sm font-semibold">Últimas noticias</h2>
+      {newsFetchStatus === "pending" ?//Loading
+        (
+          <SkeletonCarousel />
+        )
+        :
+        newsFetchStatus === "rejected" ?//Error
+          (<h1>Hubo un error</h1>)
+          ://Success
+          (
+            <div className="overflow-hidden w-full bg-black">
+              <Slider {...carouselSettings}>
+                {arrayNews.map((notice, index) => (
+                  <img key={index} src={notice.image} alt="" className="h-36 bg" />
+                ))}
+              </Slider>
+            </div>
+          )
+      }
+    </section>
+
   );
 };
 
