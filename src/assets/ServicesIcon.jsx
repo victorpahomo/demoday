f9d805f4a43b3b65853a4d535d5157b295250d2e
JulioJavier <01juliomtz@gmail.com>
2023-03-17 20:13:54 -0500

    feat: Solved bug   <a>

--- src/assets/ServicesIcon.jsx
+++ src/assets/ServicesIcon.jsx
@@ -7,9 +7,9 @@ function ServicesIcon(props) {
       className={props.className}
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         d="M3 6a3 3 0 013-3h2.25a3 3 0 013 3v2.25a3 3 0 01-3 3H6a3 3 0 01-3-3V6zm9.75 0a3 3 0 013-3H18a3 3 0 013 3v2.25a3 3 0 01-3 3h-2.25a3 3 0 01-3-3V6zM3 15.75a3 3 0 013-3h2.25a3 3 0 013 3V18a3 3 0 01-3 3H6a3 3 0 01-3-3v-2.25zm9.75 0a3 3 0 013-3H18a3 3 0 013 3V18a3 3 0 01-3 3h-2.25a3 3 0 01-3-3v-2.25z"
-        clip-rule="evenodd"
+        clipRule="evenodd"
       />
     </svg>
   );
