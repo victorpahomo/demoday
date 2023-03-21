5c51bc4d21866c236cfbc26d3c6abc20a1e813fb
JulioJavier <01juliomtz@gmail.com>
2023-03-20 20:55:43 -0500

    feat: added functions to profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,123 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { UserIcon } from "../assets";
+import MainLayout from "../layout/MainLayout";
+import CameraIcon from "../assets/CameraIcon";
+import { uploadFileToStorage } from "../services/storageFirebaseService";
+import { setProfilePicture } from "../services/dataFirebaseService";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const email = useSelector((state) => state.user.user?.email);
+  const name = useSelector((state) => state.user.user?.name);
+  const picture = useSelector((state) => state.user.user?.picture);
+  const userUid = useSelector((state) => state.auth.user?.uid);
+  const [photo, setPhoto] = useState(null);
+  const [successPhoto, setSuccessPhoto] = useState(null);
 
-export default Profile
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const photoURL = await uploadFileToStorage(photo, userUid + photo.name);
+      console.log("photoURL", photoURL);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // async function handlePhotoChange(event) {
+  //   console.log("file", event.target.files[0]);
+  //   setPhoto(event.target.files[0]);
+  //   console.log("Photo:", photo);
+  //   // console.log(photo.name);
+  //   console.log(
+  //     await uploadFileToStorage(photo, userUid + event.target.files[0].name)
+  //   );
+  //   // successPhoto(await uploadFileToStorage(photo, userUid + photo.name));
+  //   console.log("Photo:", photo);
+  //   // setProfilePicture()
+  // }
+
+  // useEffect(() => {
+  //   if (successPhoto) {
+  //     setProfilePicture(successPhoto, userUid);
+  //   }
+  // }, [successPhoto]);
+
+  return (
+    <MainLayout props="Perfil">
+      <div className="flex flex-col relative justify-center items-center">
+        <div className="w-full h-44 bg-slate-50 rounded-xl"></div>
+        <div className="flex flex-col justify-center items-center absolute top-28">
+          <div className=" relative w-32 h-32 rounded-full bg-slate-200 shadow-lg">
+            {picture ? (
+              <img src={picture} />
+            ) : (
+              <div className="flex w-full h-full justify-center items-center">
+                <UserIcon className="fill-gray-400 w-20 h-20" />
+              </div>
+            )}
+            <label htmlFor="my-modal-4">
+              <CameraIcon
+                className="inline-block transform hover:scale-110 transition duration-300 ease-in-out  fill-gray-400 w-8 h-8 absolute bottom-0 right-0
+              hover:fill-gray-500 cursor-pointer"
+              />
+            </label>
+          </div>
+
+          <h2 className="text-xl font-semibold text-slate-600">{name}</h2>
+          <p className="text-base font-semibold text-slate-400">{email}</p>
+        </div>
+      </div>
+      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+      <label htmlFor="my-modal-4" className="modal cursor-pointer">
+        <label
+          className="flex flex-col justify-center items-center gap-5 modal-box relative bg-white"
+          htmlFor=""
+        >
+          <div className="flex flex-col justify-center items-center">
+            <div className=" w-32 h-32 rounded-full bg-slate-200">
+              {picture ? (
+                <div className="relative">
+                  <img src={picture} />
+                </div>
+              ) : (
+                <div className="relative flex w-full h-full justify-center items-center">
+                  <UserIcon className="fill-gray-400 w-20 h-20" />
+                </div>
+              )}
+            </div>
+            <h2 className="text-xl font-semibold text-slate-600">{name}</h2>
+            <p className="text-base font-semibold text-slate-400">{email}</p>
+          </div>
+          <form onSubmit={handleSubmit} action="">
+            <input
+              onChange={(e) => setPhoto(e.target.files[0])}
+              id="SearchPhotoProfile"
+              type="file"
+              className="bg-slate-100 file-input file-input-bordered file-input-info w-full max-w-xs"
+            />
+            <div className="flex justify-between mt-5 gap-5 ">
+              <button
+                onClick={() => {
+                  document.getElementById("my-modal-4").checked = false;
+                }}
+                className="btn bg-primary-azulCeleste4 border-none text-white hover:bg-green-500"
+                type="submit"
+              >
+                Guardar
+              </button>
+              <label
+                htmlFor="my-modal-4"
+                className="btn bg-primary-azulCeleste4 hover:bg-red-500 border-none text-white"
+              >
+                Salir
+              </label>
+            </div>
+          </form>
+        </label>
+      </label>
+    </MainLayout>
+  );
+};
+
+export default Profile;
