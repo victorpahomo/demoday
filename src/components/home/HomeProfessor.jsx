fd951186bf648101c833a24791102ba6490eb7ee
JulioJavier <01juliomtz@gmail.com>
2023-03-22 00:30:30 -0500

    feat: added professor page

--- src/components/home/HomeProfessor.jsx
+++ src/components/home/HomeProfessor.jsx
@@ -1,36 +1,56 @@
+import { useState } from "react";
 import Calendar from "../../components/home/calendar/CalendarFull";
 
 const HomeProfessor = () => {
-    return (
-        <>
-            {/* Componente para novedades */}
-            <section id='professorNews' className=" bg-white py-5 rounded-2xl border-4">
-                <div>
-                    <div className="flex justify-around">
-                        <h1 className="font-medium">Novedades</h1>
-                        <h1 className="font-medium">Link a la proxima clase</h1>
-                    </div>
-                    <h1 className="mt-5 text-3xl font-medium text-center">
-                        Ultima novedad
-                    </h1>
-                    <div className="flex justify-end">
-                        <span className="btn mr-20">ver mas</span>
-                    </div>
-                </div>
-            </section>
+  const [groupSeleted, setGroupSeleted] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const value = e.target.value.toUpperCase();
+    setGroupSeleted(value);
+  };
+  console.log("groupSeleted: ", groupSeleted);
+  return (
+    <>
+      {/* Componente para novedades */}
+      <div className="flex flex-wrap text-primary-Azul5 gap-2 justify-evenly ">
+        <select
+          defaultValue="Elije el grupo que deseas administrar"
+          onChange={handleSubmit}
+          className="select w-full max-w-xs bg-white shadow-md"
+        >
+          <option disabled value="Elije el grupo que deseas administrar">
+            Elije el grupo que deseas administrar
+          </option>
+          <option value="frontend_1">FrontEnd 1</option>
+          <option value="frontend_2">FrontEnd 2</option>
+          <option value="frontend_3">FrontEnd 3</option>
+          <option value="backend_1">BackEnd 1</option>
+          <option value="backend_2">BackEnd 2</option>
+          <option value="backend_3">BackEnd 3</option>
+        </select>
+        <div className="flex gap-2 text-primary-azulCeleste5">
+          <p>Grupo actual:</p>
+          <span className="font-bold text-primary-azulCeleste4">
+            {groupSeleted ? (
+              groupSeleted
+            ) : (
+              <p className="text-red-500 text-lg">"Selecciona un grupo"</p>
+            )}
+          </span>
+        </div>
+      </div>
 
-            {/* Componente para Calendario */}
-            <section id='professorCalendar' className="mt-10">
-                <div className="w-full">
-                    <Calendar />
-                </div>
-            </section>
+      {/* Componente para Calendario */}
+      <section id="professorCalendar" className="mt-10">
+        <div className="w-full">
+          <Calendar />
+        </div>
+      </section>
 
-            {/* Componente para proceso */}
-            <section id='professorProcess'>
-            </section>
-        </>
-    )
-}
+      {/* Componente para proceso */}
+      <section id="professorProcess"></section>
+    </>
+  );
+};
 
-export default HomeProfessor
+export default HomeProfessor;
